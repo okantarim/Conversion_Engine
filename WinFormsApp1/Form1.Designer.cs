be73ce10608ae1fa39a3a6d55ddfe1588d02ff3d
okantarim <100791682+okantarim@users.noreply.github.com>
2022-08-29 22:01:04 +0300

    Add files via upload

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.cmbBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(100, 56);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(125, 34);
+            this.txtBox1.TabIndex = 0;
+            // 
+            // cmbBox1
+            // 
+            this.cmbBox1.FormattingEnabled = true;
+            this.cmbBox1.Items.AddRange(new object[] {
+            "string",
+            "int",
+            "double",
+            "float",
+            "char"});
+            this.cmbBox1.Location = new System.Drawing.Point(348, 56);
+            this.cmbBox1.Name = "cmbBox1";
+            this.cmbBox1.Size = new System.Drawing.Size(151, 36);
+            this.cmbBox1.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(100, 175);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 28);
+            this.lbl1.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(604, 56);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(116, 36);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(772, 56);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 34);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 630);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.cmbBox1);
+            this.Controls.Add(this.txtBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.ComboBox cmbBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to the conversion engine");
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (cmbBox1.SelectedItem == "string")
+            {
+                lbl1.Text = txtBox1.Text;
+            }
+            else if (cmbBox1.SelectedItem == "int")
+            {
+                try
+                {
+                    int number1 = Convert.ToInt32(txtBox1.Text);
+                    lbl1.Text = Convert.ToString(number1);
+                }
+                catch (FormatException)
+                {
+
+                    MessageBox.Show("Please enter an integer value.");
+                }
+            }
+            else if (cmbBox1.SelectedItem == "double")
+            {
+                try
+                {
+                    double number2 = Convert.ToDouble(txtBox1.Text);
+                    lbl1.Text = Convert.ToString(number2);
+                }
+                catch (FormatException)
+                {
+
+                    MessageBox.Show("Please enter an double type data.");
+                }
+            }
+            else if (cmbBox1.SelectedItem == "float")
+            {
+                try
+                {
+                    float number4 = float.Parse(txtBox1.Text);
+                    lbl1.Text = Convert.ToString(number4);
+                }
+                catch (FormatException)
+                {
+
+                    MessageBox.Show("Please enter a float type data.");
+                }
+            }
+            else if (cmbBox1.SelectedItem == "char")
+            {
+                try
+                {
+                    char[] number3 = txtBox1.Text.ToCharArray();
+                    lbl1.Text = Convert.ToString(number3);
+                }
+                catch (FormatException)
+                {
+
+                    MessageBox.Show("Please enter a char type data.");
+                }
+                
+
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
